55c76f95d4f0b5c0c2ae86e9a10d088c9f1d1e36
saurabhkumar13 <saurabhkumar1311@gmail.com>
2017-08-21 17:26:39 +0530

    Test conversions of all pathways to 2017, BioPAXStrict, SIF

---
+++ src/org/pathvisio/core/exporter/BiopaxExporterStrict.java
@@ -0,0 +1,117 @@
+package org.pathvisio.core.exporter;
+
+import com.sun.xml.bind.Util;
+import org.biopax.paxtools.model.BioPAXFactory;
+import org.biopax.paxtools.model.BioPAXLevel;
+import org.biopax.paxtools.model.Model;
+import org.biopax.paxtools.model.level3.BioSource;
+import org.bridgedb.DataSource;
+import org.bridgedb.Xref;
+import org.pathvisio.core.debug.Logger;
+import org.pathvisio.core.model.*;
+import org.pathvisio.core.network.Edge;
+import org.pathvisio.core.network.Node;
+import org.pathvisio.core.network.PathwayAsNetwork;
+import org.pathvisio.core.util.FileUtils;
+import org.pathvisio.core.util.Utils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class BiopaxExporter extends ExportHelper{
+
+    private PathwayAsNetwork panPwy;
+
+    BiopaxExporter(Pathway pathway){
+        this.pvPwy = pathway;
+        factory = BioPAXLevel.L3.getDefaultFactory();
+        bpModel = factory.createModel();
+        panPwy = new PathwayAsNetwork(pvPwy);
+
+        HashSet<PathwayElement> toKeep = new HashSet<>(), toRemove = new HashSet<>();
+        for(Edge edge:panPwy.getEdges()){
+            for(Node node:edge.getSources())
+                toKeep.addAll(node.getDataNodes());
+            for(Node node:edge.getTargets())
+                if(!node.isReactionNode())
+                    toKeep.addAll(node.getDataNodes());
+        }
+
+        Node mw = panPwy.getDataNodes().get(null);
+        for(PathwayElement pe: pvPwy.getDataObjects())
+            if(!toKeep.contains(pe)&&pe.getObjectType()==ObjectType.DATANODE&&!pe.getDataNodeType().equals(DataNodeType.PATHWAY.toString()))
+                toRemove.add(pe);
+        for(PathwayElement pe: toRemove)
+            pvPwy.remove(pe);
+        mapPathway();
+    }
+
+    private void mapPathway()
+    {
+        PathwayElement info = pvPwy.getMappInfo();
+
+        bpPwy = bpModel.addNew (org.biopax.paxtools.model.level3.Pathway.class, generateRdfId());
+        transferComments(bpPwy, info);
+        bpPwy.setDisplayName(info.getMapInfoName());
+
+//        Use first 25 characters of the Pathway name for WARNING: too.long.display.name
+//        recommended maximum is 25 characters
+
+//        bpPwy.setDisplayName(info.getMapInfoName().substring(0,25));
+
+        if (info.getOrganism() != null)
+        {
+            organism = bpModel.addNew (BioSource.class, generateRdfId());
+            organism.setStandardName(info.getOrganism());
+            organism.setDisplayName(info.getOrganism());
+            bpPwy.setOrganism(organism);
+        }
+
+        for (PathwayElement pwElm : pvPwy.getDataObjects())
+        {
+            // is it a gene, protein or metabolite?
+            if (pwElm.getObjectType() == ObjectType.DATANODE)
+            {
+                createOrGetPhysicalEntity (pwElm);
+            }
+
+            // is it a complex (NB not every group automatically counts as a complex)
+            if (pwElm.getObjectType() == ObjectType.GROUP &&
+                    pwElm.getGroupStyle() == GroupStyle.COMPLEX)
+            {
+                createOrGetPhysicalEntity (pwElm);
+            }
+
+            // is it a biochemical reaction or other relation?
+            if(isRelation(pvPwy, pwElm))
+            {
+                mapRelation(pwElm);
+            }
+
+            // is it a line that can not be represented in BioPAX?
+            if (pwElm.getObjectType() == ObjectType.LINE){
+                if((pwElm.getStartGraphRef() == null) || (pwElm.getEndGraphRef() == null)){
+                    Logger.log.info("This pathway contains an incorrect arrow");
+                }
+            }
+        }
+    }
+
+    public void export(File file, boolean doBpSs) throws IOException
+    {
+        exporter.convertToOWL(bpModel,
+                new BufferedOutputStream(new FileOutputStream(file)));
+        if (doBpSs)
+        {
+            File fnSs = FileUtils.replaceExtension(file, "bpss");
+            FileOutputStream fos = new FileOutputStream (fnSs);
+            bpss.write(fos);
+            fos.close();
+        }
+    }
+
+}
---
+++ test/org/pathvisio/core/model/TestConversions.java
@@ -0,0 +1,80 @@
+// PathVisio,
+// a tool for data visualization and analysis using Biological Pathways
+// Copyright 2006-2011 BiGCaT Bioinformatics
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package org.pathvisio.core.model;
+
+import junit.framework.TestCase;
+import org.pathvisio.core.exporter.BiopaxExporter;
+import org.pathvisio.core.exporter.BiopaxExporterStrict;
+import org.pathvisio.core.exporter.SIFExporter;
+import org.pathvisio.core.util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestConversions extends TestCase
+{
+	private static final File BASEDIR = new File ("./testData/Pathways20170810/");
+
+	public void testTo2017() throws ConverterException
+	{
+		for(File pathwaysFolder: new File (BASEDIR,"2013a").listFiles())
+			for(File in:pathwaysFolder.listFiles()){
+				Pathway pwy = new Pathway();
+				pwy.readFromXml(in, true);
+				File out = new File(BASEDIR+"/2017/"+pathwaysFolder.getName(), in.getName());
+				GpmlFormat2017.GPML_2017.writeToXml(pwy, out, true);
+			}
+	}
+
+	public void testToBioPAX() throws ConverterException, IOException
+	{
+		for(File pathwaysFolder: new File (BASEDIR,"2013a").listFiles())
+			for(File in:pathwaysFolder.listFiles()){
+				Pathway pwy = new Pathway();
+				pwy.readFromXml(in, true);
+				File out = new File(BASEDIR+"/BioPAX/"+pathwaysFolder.getName(), in.getName());
+				FileUtils.replaceExtension(out, "owl");
+				BiopaxExporterStrict biopaxExporter = new BiopaxExporterStrict(pwy);
+				biopaxExporter.export(out,false);
+			}
+	}
+
+	public void testToBioPAXOne() throws ConverterException, IOException
+	{
+		String path = "./testData/Pathways20170810/2013a/wikipathways-20170810-gpml-Bos_taurus/Bt_Folate_Metabolism_WP1075_86818.gpml";
+		File in = new File (path);
+		Pathway pwy = new Pathway();
+		pwy.readFromXml(in, true);
+		File out = File.createTempFile("temp","owl");
+		BiopaxExporterStrict biopaxExporter = new BiopaxExporterStrict(pwy);
+		biopaxExporter.export(out,false);
+	}
+
+	public void testToSIF() throws ConverterException, IOException
+	{
+		for(File pathwaysFolder: new File (BASEDIR,"2013a").listFiles())
+			for(File in:pathwaysFolder.listFiles()){
+				Pathway pwy = new Pathway();
+				pwy.readFromXml(in, true);
+				File out = new File(BASEDIR+"/SIF/"+pathwaysFolder.getName(), in.getName());
+				FileUtils.replaceExtension(out, "sif");
+				SIFExporter sifExporter = new SIFExporter(pwy);
+				sifExporter.export(out);
+			}
+	}
+
+}
